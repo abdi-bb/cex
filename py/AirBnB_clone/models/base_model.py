7423ffba3f65e058e9808bdce988994ed9b5f136
Abdi Berhe <abdi201380@gmail.com>
2023-05-10 03:55:59 -0700

    adding update

--- py/AirBnB_clone/models/base_model.py
+++ py/AirBnB_clone/models/base_model.py
@@ -10,15 +10,9 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key != '__class__':
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
