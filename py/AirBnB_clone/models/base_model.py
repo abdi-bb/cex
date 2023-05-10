107165fd5cb12a01d71289c3d9eeb7aa67f26fc4
Abdi Berhe <abdi201380@gmail.com>
2023-05-10 03:38:58 -0700

    adding update

--- py/AirBnB_clone/models/base_model.py
+++ py/AirBnB_clone/models/base_model.py
@@ -4,27 +4,32 @@ module: 'base_model'
 class: BaseModel
 '''
 
+
 import uuid
 from datetime import datetime
 
-
-class BaseModel():
-    '''base class'''
-    
-    def __init__(self) -> None:
-        self.id = str(uuid.uuid4)
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        return f'[{type(self).__name__}] ({self.id}) {self.__dict__}'
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        my_dict = self.__dict__.copy
+        my_dict = self.__dict__.copy()
         my_dict['__class__'] = type(self).__name__
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
         return my_dict
+
