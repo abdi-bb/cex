afb8e50937e798d6b699d34d2343a740a39748f1
Abdi Berhe <abdi201380@gmail.com>
2023-05-10 23:37:47 -0700

    updating basemodel to create obj from dict

--- py/AirBnB_clone/models/base_model.py
+++ py/AirBnB_clone/models/base_model.py
@@ -4,15 +4,22 @@ module: 'base_model'
 class: BaseModel
 '''
 
-
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if k in ('created_at', 'updated_at'):
+                        v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
